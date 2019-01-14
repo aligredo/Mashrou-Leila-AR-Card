5f8212e467b1e9e9e854b5174c5cebb51ffe3e2f
aligredo <aliahmedismail37@gmail.com>
2019-01-15 00:36:36 +0200

    added controlling logic

---
+++ MashrouLeilaARCard/Assets/Scripts/AlbumARCard.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class AlbumARCard : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject aoede, threeminutes, djin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aoede.SetActive(false);
+        threeminutes.SetActive(false);
+        djin.SetActive(false);
+
+        VirtualButtonBehaviour[] vrb = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for(int i = 0; i < vrb.Length; i++){
+            vrb[i].RegisterEventHandler(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb){
+        if(vb.VirtualButtonName == "01-Aoede"){
+            aoede.SetActive(true);
+            threeminutes.SetActive(false);
+            djin.SetActive(false);
+        }
+        else{
+            if(vb.VirtualButtonName == "02-3Minutes"){
+             aoede.SetActive(false);
+             threeminutes.SetActive(true);
+             djin.SetActive(false);
+            }
+            else{
+               if(vb.VirtualButtonName == "03-Djin"){
+                 aoede.SetActive(false);
+                 threeminutes.SetActive(false);
+                 djin.SetActive(true);
+             }
+            }
+            
+        }
+    }
+
+     public void OnButtonReleased(VirtualButtonBehaviour vb){
+
+    }
+}
